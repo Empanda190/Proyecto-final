65013d5d01fef8ac0a61e27fba56b91f6a1da332
KarlaSolorzanoG <karla.sg.upmor@uppuebla.edu.mx>
2024-04-16 00:00:46 -0600

    version avance de gestion de errores

--- Gestor/Errores/GestionErrores.java
+++ Gestor/Errores/GestionErrores.java
@@ -1,4 +1,5 @@
 package Gestor.Errores;
+import Gestor.Archivo.*;
 import java.util.*;
 
 public final class GestionErrores {
@@ -6,10 +7,20 @@ public final class GestionErrores {
     private boolean ExisteError;
     private int NoError;
     private String DescripcionTecnica;
+    private String[] listaErrores=new String[10]; //Creamos la lista
+    private ArchivoTexto archivo; //Creamos el objeto
+
     public GestionErrores(){
         //HashMap obtiene herencia de Map, map fue echa para ser heredable
         //tema de collection
         error = new HashMap<Integer, String>();
+        archivo=new ArchivoTexto("C:\\archivos\\errores.txt");
+
+        listaErrores[0]="Empleado no identificado";
+        listaErrores[1]="Acceso Denegado";
+        listaErrores[2]="Contraseña Errónea";
+        listaErrores[3]="Empleado no encontrado al momento de registrar su contrato";
+        listaErrores[4]="Empleado no encontrado al momento de buscar un contrato";
     }
     private void CargarErrores(){
         //...
@@ -17,8 +28,11 @@ public final class GestionErrores {
     public void setNoError(int noError) {
         NoError = noError;
     }
-    public String getError(){
-        return "p";
+    public String getError(int id){
+        archivo.AbrirModoEscritura();
+        archivo.escribir(listaErrores[id]);
+        archivo.cerrar();
+        return listaErrores[id];
     }
     public String getErrorTecnico(){
         return "r";
