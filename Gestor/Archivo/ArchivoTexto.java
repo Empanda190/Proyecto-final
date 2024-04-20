e138db8982bfcfb2c533e7f8f259d0a516acdab0
KarlaSolorzanoG <karla.sg.upmor@uppuebla.edu.mx>
2024-04-20 14:45:28 -0600

    Versión Archivo detalles 2

--- Gestor/Archivo/ArchivoTexto.java
+++ Gestor/Archivo/ArchivoTexto.java
@@ -126,7 +126,7 @@ public final class ArchivoTexto extends ControlArchivos implements iFileText {
             } else {
                 // Abre el archivo en modo de lectura
                 FileReader reader = new FileReader(a);
-                this.bufferedReader = new BufferedReader(reader);
+                this.br = new BufferedReader(reader);
                 this.ModoLectura = true;
             }
             this.ArchivoExiste = true;
